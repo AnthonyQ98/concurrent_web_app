e4edb0b7ca955dc5300c39a0cf6443ce993f5325
Anthony <anthonyquinn.developer@gmail.com>
2024-03-29 21:11:09 +0000

    Adding goroutines

--- server/server.go
+++ server/server.go
@@ -10,6 +10,12 @@ import (
 func StartServer() {
 	log.Println("Starting webserver on port 8080...")
 	http.HandleFunc("/home", handlers.Home)
-	err := http.ListenAndServe("localhost:8080", nil)
-	utilities.Check(err)
+	// Start a goroutine to serve incoming HTTP requests concurrently
+	go func() {
+		err := http.ListenAndServe("localhost:8080", nil)
+		utilities.Check(err)
+	}()
+
+	// Using blocking select statement for simplicity
+	select {}
 }
