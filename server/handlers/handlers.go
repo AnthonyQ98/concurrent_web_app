e2c547929af70916ecc66985828f515c8083ed50
Anthony <anthonyquinn.developer@gmail.com>
2024-03-29 21:08:24 +0000

    Rendering html/templates

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -7,10 +7,34 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	Name  string
+	Price float32
+}
+
+func NewProduct(name string, price float32) Product {
+	if name == "" {
+		name = "Foobar"
+	}
+	if price < 0 {
+		price = 10.0
+	}
+	return Product{
+		Name:  name,
+		Price: price,
+	}
+}
+
+func productBuilder() []Product {
+	products := []Product{NewProduct("Jim", 12.0), NewProduct("Bob", -10)}
+	return products
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
+	products := productBuilder()
 	res, err := template.ParseFiles("templates/home.html")
 	utilities.Check(err)
-	err = res.Execute(w, nil)
+	err = res.Execute(w, products)
 	utilities.Check(err)
 	log.Println("Successful request!")
 }
